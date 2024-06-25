a5ca7d7873a6533f8f4f21ddb279c1bd8bfa29a3
Dima65584656 <162564519+Dima65584656@users.noreply.github.com>
2024-06-25 04:45:20 +0300

    Alpha version 0.9

--- FRONTEND/index.js
+++ FRONTEND/index.js
@@ -1,18 +1,24 @@
-var lastQuantity = 1; // Переменная для хранения последнего введенного значения
+var lastQuantity = {}; // Объект для хранения последних введенных значений
 
 // Функция для обновления общей цены
 function updateTotalPrice(input) {
-    var quantityInput = document.getElementById('quantity-input');
-    var quantity = quantityInput.value !== '' ? parseInt(quantityInput.value) : lastQuantity; // Проверяем, если значение не пустое, то преобразуем его в число, иначе используем 0
-    var totalPrice = calculateTotalPrice(quantity); // Вызываем функцию для расчета общей суммы с учетом количества
-    document.getElementById('total-price').textContent = totalPrice + '\u20BD'; // Обновляем текст общей цены
-    document.getElementById('total-price-all').textContent = totalPrice + '\u20BD'; // Обновляем текст итоговой цены
-    lastQuantity = quantity; // Сохраняем текущее значение как lastQuantity
+    var parent = input.closest('.buy-item');
+    var quantityInput = parent.querySelector('.quantity-input');
+    var quantity = quantityInput.value !== '' ? parseInt(quantityInput.value) : lastQuantity[parent.id] || 1; // Проверяем, если значение не пустое, то преобразуем его в число, иначе используем 0
+    var pricePerItem = parseInt(parent.getAttribute('data-price'));
+    
+    var totalPrice = calculateTotalPrice(quantity, pricePerItem); // Вызываем функцию для расчета общей суммы с учетом количества
+    parent.querySelector('.total-price').textContent = totalPrice + '\u20BD'; // Обновляем текст общей цены
+    
+    var totalPrices = document.querySelectorAll('.total-price');
+    var sum = Array.from(totalPrices).reduce((acc, elem) => acc + parseInt(elem.textContent), 0);
+    
+    document.querySelector('.total-price-all').textContent = sum + '\u20BD'; // Обновляем текст итоговой цены
+    lastQuantity[parent.id] = quantity; // Сохраняем текущее значение как lastQuantity
 }
 
 // Функция для расчета общей цены
-function calculateTotalPrice(quantity) {
-    var pricePerItem = 3000; // Цена одного товара
+function calculateTotalPrice(quantity, pricePerItem) {
     return pricePerItem * quantity; // Рассчитываем общую цену
 }
 
@@ -43,30 +49,26 @@ function increaseValue(input) {
 }
 
 // Функция, определяет пуста ли корзина
-function hideObject() {
-    var objects = document.querySelectorAll(".object");
+function hideItem(button) {
+    var item = button.closest(".buy-item");
+    item.style.display = "none";
+  
+    var itemsVisible = document.querySelectorAll(".buy-item:not([style='display: none;'])");
     var message = document.getElementById("message");
   
-    for (var i = 0; i < objects.length; i++) {
-      if (objects[i].style.display !== "none") {
-        objects[i].style.display = "none";
-        break;
-      }
-    }
-  
-    var objectsVisible = document.querySelectorAll(".object:not([style='display: none;'])");
-  
-    if (objectsVisible.length === 0) {
-      document.getElementById("objects").style.display = "none";
-      document.getElementById("btn").style.display = "none";
-      document.getElementById("message").style.display = "block";
+    if (itemsVisible.length === 0) {
+      document.getElementById("items").style.display = "none";
+      document.getElementById("menu").style.display = "none";
+      message.style.display = "block";
     }
 }
+
+
 
 document.querySelectorAll('.minus-btn').forEach(function(btn) {
     btn.addEventListener('click', function(e) {
         e.preventDefault();
-        var input = this.closest('div').querySelector('input');
+        var input = this.closest('.buy-item').querySelector('.quantity-input');
         decreaseValue(input);
     });
 });
@@ -74,17 +76,19 @@ document.querySelectorAll('.minus-btn').forEach(function(btn) {
 document.querySelectorAll('.plus-btn').forEach(function(btn) {
     btn.addEventListener('click', function(e) {
         e.preventDefault();
-        var input = this.closest('div').querySelector('input');
+        var input = this.closest('.buy-item').querySelector('.quantity-input');
         increaseValue(input);
     });
 });
 
-document.getElementById('quantity-input').addEventListener('input', function() {
-    var quantityInput = document.getElementById('quantity-input');
-    var quantity = parseInt(quantityInput.value);
-    if (quantity > 100) {
-        quantity = 100; // Ограничиваем введенное число 100, если оно больше
-        quantityInput.value = quantity; // Устанавливаем новое значение в input-поле
-    }
-    updateTotalPrice(); // Вызываем функцию для обновления общей суммы
+document.querySelectorAll('.quantity-input').forEach(function(input) {
+    input.addEventListener('input', function() {    
+        var quantityInput = this;
+        var quantity = parseInt(quantityInput.value);
+        if (quantity > 100) {
+            quantity = 100; // Ограничиваем введенное число 100, если оно больше
+            quantityInput.value = quantity; // Устанавливаем новое значение в input-поле
+        }
+        updateTotalPrice(quantityInput); // Вызываем функцию для обновления общей суммы
+    });
 });
