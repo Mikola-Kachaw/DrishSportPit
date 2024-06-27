a861714d5b419e8eef3b4c52c7d55ed1d56698b5
natascha1132 <100133503+natascha1132@users.noreply.github.com>
2024-06-27 14:37:23 +0300

    BACKEND

---
+++ ServerPractic/DBContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ServerPractic.Entities;
+
+
+
+namespace ServerPractic
+{
+    public class DBContext: DbContext
+    {
+        public DbSet<ProductEntity> Product => Set<ProductEntity>();
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlite("Filename=database.db");
+        }
+
+    }
+}
---
+++ ServerPractic/Program.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using ServerPractic;
+using ServerPractic.Entities;
+
+
+
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<DBContext>();
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<CartService>();
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
