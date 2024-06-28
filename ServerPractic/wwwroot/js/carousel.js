300ada7e35db177e5c59664ea511f759b8fb7836
natascha1132 <100133503+natascha1132@users.noreply.github.com>
2024-06-28 14:57:26 +0300

    BACKEND

--- ServerPractic/wwwroot/js/carousel.js
+++ ServerPractic/wwwroot/js/carousel.js
@@ -1,29 +1,34 @@
 var slidePosition = 2;
 
 function SlideShow(n) {
-  var i;
-  var slides = document.getElementsByClassName("Containers");
-  if (n > slides.length) {slidePosition = 1}
-  if (n < 1) {slidePosition = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  slides[slidePosition-1].style.display = "block";
+    var i;
+    var slides = document.getElementsByClassName("Containers");
+    if (n > slides.length) { slidePosition = 1 }
+    if (n < 1) { slidePosition = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+        slides[i].classList.remove('fade'); // удаляем анимации, чтобы избежать конфликтов
+    }
+    slides[slidePosition - 1].style.display = "block";
+    slides[slidePosition - 1].classList.add('fade'); // добавляем анимацию к текущему слайду
 }
 
 SlideShow(slidePosition);
 
 function plusSlides(n) {
-    let currentSlide = slidePosition; // сохраняем текущий слайд
+    let currentSlide = slidePosition;
     SlideShow(slidePosition += n);
-  
+
     // Прерываем текущую анимацию и моментально переходим к следующему или предыдущему слайду
-    document.querySelector('.Containers:nth-child(' + currentSlide + ')').classList.remove('fade');
-    document.querySelector('.Containers:nth-child(' + currentSlide + ')').style.opacity = 0;
-  
-    // Восстанавливаем анимацию для нового слайда
-    document.querySelector('.Containers:nth-child(' + slidePosition + ')').classList.add('fade');
-  }
+    let slides = document.getElementsByClassName("Containers");
+    if (currentSlide > 0 && currentSlide <= slides.length) {
+        slides[currentSlide - 1].style.opacity = 0;
+    }
+    if (slidePosition > 0 && slidePosition <= slides.length) {
+        slides[slidePosition - 1].style.opacity = 1;
+        slides[slidePosition - 1].classList.add('fade');
+    }
+}
 
 function autoSlide() {
     plusSlides(1); // вызываем функцию для перехода к следующему слайду
@@ -32,3 +37,14 @@ function autoSlide() {
 
 // Запускаем автоматическую смену слайдов
 autoSlide();
+
+// Инициализация начального состояния слайдов из Blazor
+function initSlides() {
+    slidePosition = 1;
+    SlideShow(slidePosition);
+}
+
+// Запуск автоматической смены слайдов из Blazor
+function startAutoSlide() {
+    autoSlide();
+}
